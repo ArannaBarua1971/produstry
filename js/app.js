a6a8c0dcf4391a633ef6e157107e456a9f1a710c
ArannaBaura1971 <arannabaruase71@gmail.com>
2022-09-23 21:18:45 +0600

    changes

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    // --> for banner slider
+    $('.slider').slick({
+        prevArrow:'<div class="slider-icon" id="left-icon"><i class="icon fas fa-chevron-left"></i></div>',
+        nextArrow:'<div class="slider-icon" id="right-icon"><i class="icon fas fa-chevron-right"></i></div>',
+        autoplay:true,
+    })
+})
