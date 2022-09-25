de58b8615431defe35b2d4bed139c4a4c3bf9c3c
ArannaBarua1971 <arannabaruase71@gmail.com>
2022-09-25 19:28:40 +0600

    changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// jquery link
 $(document).ready(function(){
 
     // --> for banner slider
@@ -6,4 +7,28 @@ $(document).ready(function(){
         nextArrow:'<div class="slider-icon" id="right-icon"><i class="icon fas fa-chevron-right"></i></div>',
         autoplay:true,
     })
+
+    // --> about path slider
+    $('#company').on('click',function(){
+        $('#about .about_content .content_path_menu .content_path_slider').animate({margin:'0px 0px 0px 0px'})
+    })
+    $('#history').on('click',function(){
+        $('#about .about_content .content_path_menu .content_path_slider').animate({margin:'0px 0px 0px 130px'})
+    })
+    $('#mission').on('click',function(){
+        $('#about .about_content .content_path_menu .content_path_slider').animate({margin:'0px 0px 0px 260px'})
+    })
+    $('#awards').on('click',function(){
+        $('#about .about_content .content_path_menu .content_path_slider').animate({margin:'0px 0px 0px 370px'})
+    })
 })
+// --> about section js
+function show(v1,v2,v3,v4){
+    $(v1).removeClass('display-none')
+    $(v2).addClass('display-none')
+    $(v3).addClass('display-none')
+    $(v4).addClass('display-none')
+}
+
+
+ 
