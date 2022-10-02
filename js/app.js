7fd5788cc5af33d40c458a185042e3c51a2c3873
ArannaBaura1971 <arannabaruase71@gmail.com>
2022-10-03 00:42:37 +0600

    dsffsd

--- js/app.js
+++ js/app.js
@@ -10,7 +10,12 @@ $(document).ready(function () {
         $('.hide_everything').animate({width:'0%'})
         $('.menu_bar_show').animate({right:'-250px'})
     })
+    $('.nav_btn').on('click',function(){
+        $('.menu').toggleClass('showHide')
+        $('#navbar .logo').toggleClass('left_side')
+    })
 
+    
     // --> for banner slider
     $('.slider').slick({
         prevArrow: '<div class="slider-icon" id="left-icon"><i class="icon fas fa-chevron-left"></i></div>',
