e94afa55811fc02c59379c85d42eb1dd4f6586a7
ArannaBaura1971 <arannabaruase71@gmail.com>
2022-10-05 00:08:21 +0600

    fdasfd

--- js/app.js
+++ js/app.js
@@ -2,24 +2,23 @@
 $(document).ready(function () {
 
     // navbar js 
-    $('.handbar .show').on('click',function(){
-        $('.menu_bar_show').animate({right:'0px'})
-        $('.hide_everything').animate({width:'100%'})
+    $('.handbar .show').on('click', function () {
+        $('.menu_bar_show').animate({ right: '0px' })
+        $('.hide_everything').animate({ width: '100%' })
     })
-    $('.handbar .hide').on('click',function(){
-        $('.hide_everything').animate({width:'0%'})
-        $('.menu_bar_show').animate({right:'-250px'})
+    $('.handbar .hide').on('click', function () {
+        $('.hide_everything').animate({ width: '0%' })
+        $('.menu_bar_show').animate({ right: '-250px' })
     })
-    $('.nav_btn').on('click',function(){
+    $('.nav_btn').on('click', function () {
         $('.menu').toggleClass('showHide')
         $('#navbar .logo').toggleClass('left_side')
     })
 
-    
+
     // --> for banner slider
     $('.slider').slick({
-        prevArrow: '<div class="slider-icon" id="left-icon"><i class="icon fas fa-chevron-left"></i></div>',
-        nextArrow: '<div class="slider-icon" id="right-icon"><i class="icon fas fa-chevron-right"></i></div>',
+        arrows:false,
         autoplay: true,
     })
 
@@ -28,54 +27,66 @@ $(document).ready(function () {
         $('#about .about_content .content_path_menu .content_path_slider').animate({ margin: '0px 0px 0px 0px' })
     })
     $('#history').on('click', function () {
-        $('#about .about_content .content_path_menu .content_path_slider').animate({ margin: '0px 0px 0px 30%' })
+        $('#about .about_content .content_path_menu .content_path_slider').animate({ margin: '0px 0px 0px 29%' })
     })
     $('#mission').on('click', function () {
-        $('#about .about_content .content_path_menu .content_path_slider').animate({ margin: '0px 0px 0px 59%' })
+        $('#about .about_content .content_path_menu .content_path_slider').animate({ margin: '0px 0px 0px 57%' })
     })
     $('#awards').on('click', function () {
-        $('#about .about_content .content_path_menu .content_path_slider').animate({ margin: '0px 0px 0px 80%' })
+        $('#about .about_content .content_path_menu .content_path_slider').animate({ margin: '0px 0px 0px 84%' })
     })
 
     // --> achievements counterUP
     $('.counter').counterUp({
         delay: 10,
-        time: 2000
+        time: 2000,
     });
 
     // --> blog slider
     $('.blog_item_slider').slick({
-        prevArrow: '<div class="slider-icon" id="left-icon"><i class="icon fas fa-chevron-left"></i></div>',
-        nextArrow: '<div class="slider-icon" id="right-icon"><i class="icon fas fa-chevron-right"></i></div>',
         slidesToShow: 3,
-        slidesToScroll: 1,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        autoplay:true,
     })
+
 
     // --> testimonial slider
     $('.testimonial_item_slider').slick({
-        prevArrow: '<div class="slider-icon" id="left-icon"><i class="icon fas fa-chevron-left"></i></div>',
-        nextArrow: '<div class="slider-icon" id="right-icon"><i class="icon fas fa-chevron-right"></i></div>',
-        initialSlide: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.employee_for_slide'
     });
     $('.employee_for_slide').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1990,
-        initialSlide: 0,
-        arrows: false,
+        asNavFor: '.testimonial_item_slider',
+        centerMode: true,
+        focusOnSelect: true,
+        arrows:false,
+        centerPadding:'0px',
     });
 
 
     //  --> brand slider
     $('.brand_slider').slick({
-        prevArrow: '<div class="slider-icon" id="left-icon"><i class="icon fas fa-chevron-left"></i></div>',
-        nextArrow: '<div class="slider-icon" id="right-icon"><i class="icon fas fa-chevron-right"></i></div>',
         slidesToShow: 5,
         slidesToScroll: 2,
         autoplay: true,
+        centerMode: true,
+        centerPadding: '0px',
+        arrows: false,
+    })
+
+    //  --> team slider
+    $('.team_slider').slick({
+        slidesToShow:3,
+        arrows:false,
+        centerMode:true,
+        centerPadding:'0px',
+        autoplay:true,
     })
 })
 // --> about section js
